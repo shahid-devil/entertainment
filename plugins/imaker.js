62c96b41de512ccb114f2e846abb743680cb74ec
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-10-19 15:23:44 +0530

    `imkaer added by Deadpool 😈`

--- plugins/imaker.js
+++ plugins/imaker.js
@@ -1,3 +1,9 @@
+/* Copyright (C) 2021 shahid-devil.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+LUTTAPI
+*/
+
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
